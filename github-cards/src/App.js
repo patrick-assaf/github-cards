84fa82500ceee16e9f386ec5c0f6ff5f1c65d5c1
Patrick Assaf <patrickassaf96@gmail.com>
2020-03-17 02:20:45 -0400

    Add and integrate the Card class component

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Card from './Card';
 
 class App extends React.Component {
 
   render() {
-  return <div className="header">{this.props.title}</div>
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Card />
+      </div>
+    );
   }
 }
 
---
+++ github-cards/src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class Card extends React.Component {
+
+  render() {
+      return (
+        <div className="github-profile">
+            <img alt="" src="https://placehold.it/75" />
+            <div className="info">
+                <div className="name">Name here...</div>
+                <div className="company">Company here...</div>
+            </div>
+        </div>
+      );
+  }
+}
+
+export default Card;
