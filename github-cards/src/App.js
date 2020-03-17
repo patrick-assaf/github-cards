506f1c3549ce64f29885c299a03f80b0a2aa274a
Patrick Assaf <patrickassaf96@gmail.com>
2020-03-17 02:51:12 -0400

    Restore previous css stylesheet

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import Card from './Card';
 
 class App extends React.Component {
--- github-cards/src/Card.js
+++ github-cards/src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './index.css';
 
 class Card extends React.Component {
 
   render() {
       return (
-        <div className="github-profile" style={{ margin: '1rem' }}>
+        <div className="github-profile">
             <img alt="" src="https://placehold.it/75" />
-            <div className="info" style={{ display: 'inline-block', marginLeft: 10 }}>
-                <div className="name" style={{ fontSize: '125%' }}>Name here...</div>
+            <div className="info">
+                <div className="name">Name here...</div>
                 <div className="company">Company here...</div>
             </div>
         </div>
